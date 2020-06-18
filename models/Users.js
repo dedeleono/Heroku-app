1b3b69fbc06a4d16f9e132f550ac613b4cac0f59
Diego de Leon <dienrique24@hotmail.com>
2020-06-17 21:20:20 -0500

    initial db setup

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        required: false,
+        trim: true
+    },
+
+    last_name: {
+        type: String,
+        required: false,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    is_active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const Users = mongoose.model('Users', UsersSchema);
+
+module.exports= Users;
